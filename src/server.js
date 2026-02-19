e017c52e6ec7fe69badde6145ab15d0224b4f4bb
Basharat Ali <80580890+BasharatAliKing@users.noreply.github.com>
2026-02-19 12:14:53 +0500

    df

--- src/server.js
+++ src/server.js
@@ -39,7 +39,8 @@ app.use(cors({
 }));
 
 app.use(morgan("dev")); 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use("/images", express.static("public/images"));
 app.use("/videos", express.static("public/videos"));
 app.use("/api/snapshots", snapshotRoutes);
